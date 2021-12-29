05882e2f9967b3ad39f91ca4900f66f77c6060b0
Yuiichan <tranduybinh3010@gmail.com>
2021-12-29 11:39:51 +0700

    Remove Public/css

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-const text = '<h1>Hello world</h1>'
-app.get('/home', (req, res) => res.send(text))
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
