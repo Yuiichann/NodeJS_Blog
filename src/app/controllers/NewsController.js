c5725d58c7bf0f628019aef31a26ac461ae79751
Yuiichan <tranduybinh3010@gmail.com>
2021-12-30 11:16:24 +0700

    Install prittier...

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,6 +1,4 @@
-
 class NewsController {
-
     // [GET] / news
     index(req, res) {
         res.render('news');
@@ -8,8 +6,8 @@ class NewsController {
 
     // [GET] / show news details
     show(req, res) {
-        res.send('INFO DETAIL!')
+        res.send('INFO DETAIL!');
     }
 }
 
-module.exports = new NewsController;
+module.exports = new NewsController();
